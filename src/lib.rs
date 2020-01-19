899890c7136b035be4de62468e82f14d64b35c05
Jabir-Abbas <jabir.abbas1@gmail.com>
2020-01-19 20:40:17 +0500

    Assignment 07 (Question # 2)

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod school_section{
+    pub mod junior_section{
+        pub fn ground(){
+            println!("You are in play ground of junior section");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use college::school_section::junior_section::ground;
+
+fn main() {
+ ground();
+}
